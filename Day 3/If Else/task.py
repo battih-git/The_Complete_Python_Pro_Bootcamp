176380b0a2f440a92d3f36da129ff60b127fa2d3
Huzefa Battiwala <huzefabattiwala.hb@gmail.com>
2025-01-31 16:22:16 +0530

    Day 2 Commit

--- Day 3/If Else/task.py
+++ Day 3/If Else/task.py
@@ -1,2 +1,15 @@
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
+
+if height > 120:
+    print("You can ride the rollercoaster.")
+else:
+    print("You have to grow taller before you ride.")
+
+# Comparison operator
+# > Greater than
+# < Lesser than
+# >= Greater than or equal to
+# <= Lesser than or equal to
+# == Equal to
+# != Not equal to

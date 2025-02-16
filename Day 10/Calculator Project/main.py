62bd70b61c0b3e76c937b900ab9d13794e6cca3f
Huzefa Battiwala <huzefabattiwala.hb@gmail.com>
2025-02-16 10:13:56 +0530

    Day 2 Commit

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,39 @@
+import  art
+print(art.logo)
 def add(n1, n2):
     return n1 + n2
+def subtract (n1,n2):
+    return  n1-n2
+def multiply(n1,n2):
+    return n1 * n2
+def divide(n1,n2):
+    return n1 / n2
+
+dict_operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide
+}
+
+def calculator():
+    num1 = float(input("What's the first number? "))
+    condition = True
+    while condition:
+        for key in dict_operations:
+            print(key)
+        operation = input("Pick an operation: ")
+        num2 = float(input("What's the next number? "))
+        result = dict_operations[operation](num1,num2)
+        print(f"{num1} {operation} {num2} = {result}")
+        user_confirmation = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation:")
+        if user_confirmation.lower() == 'y':
+            condition = True
+            num1 = result
+        else:
+            condition = False
+            calculator()
+
+calculator()
 
 
---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,6 @@
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return (formatted_f_name, formatted_l_name)
+formatted_str = format_name('huzefa','battiwala')
+print(formatted_str)

bba2fde537ea0c60e5855566a0032c00e99c0a22
Huzefa Battiwala <huzefabattiwala.hb@gmail.com>
2025-03-22 17:45:02 +0530

    Day 19 Commit

--- Day 19/Turtle Race/main.py
+++ Day 19/Turtle Race/main.py
@@ -9,18 +9,32 @@ user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win
 print(user_bet)
 screen.setup(width= 500,height=400)
 colors = ['red', 'orange', 'yellow','green', 'blue', 'purple']
-
+all_turtles = []
 is_race_on = False
 
 for turtle_index in range(6):
-    tim = Turtle('turtle')
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y = -100 + (30 * turtle_index))
+    new_turtle = Turtle('turtle')
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y =-100 + (30 * turtle_index))
+    all_turtles.append(new_turtle)
+
 
 if user_bet:
     is_race_on = True
 
+while is_race_on:
+    print(all_turtles)
+    for turtle in all_turtles:
+        if turtle.xcor()>230:
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+            is_race_on = False
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
 
 
 screen.exitonclick()
